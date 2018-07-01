8eee8e6756e36a0ffc19577b264c7ae789c8a92c
jcgpm <sistema3015@gmail.com>
2018-07-01 19:19:56 -0400

    primer commit

---
+++ PrimerPrograma.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "hola mundo";
+
+?>
+<html5>
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+	</head>
+	<body>
+		<<h1>hola</h1>
+	</body>
+	</html>
+</html5>
+
+
+
+
---
+++ SegundoP.php
@@ -0,0 +1,13 @@
+<?php
+echo "hola mundo codigo php";
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+  <head>
+   <meta charset="utf-8">
+   <title>primer</title>
+  </head>
+  <body>
+   <h1>hola</h1>
+  </body>
+ </html>
